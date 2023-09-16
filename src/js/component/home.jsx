4d087c8ea50b79c1217d592ccbc70d9ef1006a90
angelafort <139291628+angelafort@users.noreply.github.com>
2023-09-16 20:44:41 +0000

    landing page react done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Navbar from "./Navbar.jsx";
+import Jumbotron from "./Jumbotron.jsx";
+import Card from "./Card.jsx";
+import Footer from "./Footer.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,21 +10,26 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+		<>
+			<Navbar />
+			<div class="container">
+				<Jumbotron />
+			</div>
+			<br/>
+			<div class="container text-center">
+				<div class="row">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
+			<br/>
+			<div>
+			<Footer />
+			</div>
+		</>
+	)
+}
 
 export default Home;
